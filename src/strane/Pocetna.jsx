70776d49d7acc7aad77ec3f2e603c09536add7d8
JovanovicNena <jovanovicnenad320@gmail.com>
2022-08-30 23:44:23 +0200

    tabela za strelce premijer lige na pocetnoj strani

---
+++ src/strane/Pocetna.jsx
@@ -0,0 +1,33 @@
+
+
+function Pocetna() {
+
+
+
+
+
+    return (
+        <div className="d-pocetna">
+
+            <h1>Premier League '22 - Top Scorers</h1>
+
+            <table className="tbl">
+                <thead>
+                    <tr>
+                        <th>Ime</th>
+                        <th>Prezime</th>
+                        <th>Godine</th>
+                        <th>Broj na dresu</th>
+                        <th>Klub</th>
+                        <th>Broj golova</th>
+                    </tr>
+                </thead>
+                <tbody >
+
+                </tbody>
+            </table>
+        </div>
+    )
+}
+
+export default Pocetna;
