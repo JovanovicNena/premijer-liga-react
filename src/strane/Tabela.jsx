7dea5ebf573571a7f63a965c2dc85c46428de5dc
JovanovicNena <jovanovicnenad320@gmail.com>
2022-08-31 23:41:16 +0200

    tabela klubova popunjena podacima

--- src/strane/Tabela.jsx
+++ src/strane/Tabela.jsx
@@ -1,41 +1,13 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 
 function Tabela() {
 
-    const [data, setData] = useState();
+    const [data, setData] = useState([]);
 
 
+    function prikazTabele() {
 
-    return (
-        <div className="d-tabela">
-
-            <button className='btn btn-primary' id='premier-tbl'>Prikazi tabelu Premier lige</button>
-
-            <table className="tbl table" id="tabela" hidden>
-                <thead>
-                    <tr>
-                        <th>Naziv kluba</th>
-                        <th>Pobede</th>
-                        <th>Neresno</th>
-                        <th>Porazi</th>
-                        <th>Broj bodova</th>
-                        <th>Pozicija</th>
-                    </tr>
-                </thead>
-
-            </table>
-
-
-
-        </div>
-    )
-}
-
-export default Tabela;
-
-/*
-
- const options = {
+        const options = {
             method: 'GET',
             headers: {
                 'X-RapidAPI-Key': '1d0c4bc4afmshaeb2b5a63baf122p14654fjsn59b6ed1f2960',
@@ -45,6 +17,50 @@ export default Tabela;
 
         fetch('https://premier-league-standings1.p.rapidapi.com/', options)
             .then(response => response.json())
-            .then(response => console.log(response))
+            .then(response => setData(response))
             .catch(err => console.error(err));
-            */
+
+    }
+
+
+    return (
+        <div className="d-tabela">
+
+            <button className='btn btn-primary' onClick={prikazTabele} id='premier-tbl'>Prikazi tabelu Premier lige</button>
+
+            <div>
+                <table className="table table-bordered table-light" id="tabela" style={{ display: data.length > 0 ? 'block' : 'none' }}>
+                    <thead className="table-primary">
+                        <tr>
+                            <th>Naziv kluba</th>
+                            <th>Pobede</th>
+                            <th>Nerešeno</th>
+                            <th>Porazi</th>
+                            <th>Broj bodova</th>
+                            <th>Pozicija</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {data.map(klub => {
+                            return (
+                                <tr key={klub.team.abbreviation}>
+                                    <td>{klub.team.name} <img src={klub.team.logo} style={{ height: '50px' }} /></td>
+                                    <td>{klub.stats.wins}</td>
+                                    <td>{klub.stats.ties}</td>
+                                    <td>{klub.stats.losses}</td>
+                                    <td>{klub.stats.points}</td>
+                                    <td>{klub.stats.rank}</td>
+                                </tr>
+                            );
+                        })}
+                    </tbody>
+                </table>
+            </div>
+
+
+        </div >
+    )
+}
+
+export default Tabela;
+
